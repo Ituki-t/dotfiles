b25fbdbefc3498110f6b0787bd6d2bc9375eb545
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-08-13 14:27:18 +0900

    nvmを自動でインストールするscript

--- scripts/setup-wsl.sh
+++ scripts/setup-wsl.sh
@@ -16,4 +16,18 @@ sudo apt install -y \
   zoxide \
   tmux
 
+echo "Installing nvm..."
+
+export NVM_DIR="$HOME/.nvm"
+
+if [ ! -d "$NVM_DIR" ]; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.5/install.sh | bash
+fi
+
+# Load nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+echo "Installing Node.js LTS..."
+nvm install --lts
+
 echo "Done!"
