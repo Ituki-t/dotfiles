3ec2b33db037eba8708aabfb948a43210da777fd
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-08-16 11:19:56 +0900

    tab

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 config.default_domain = "WSL:Ubuntu-22.04"
 config.window_background_opacity = 0.8
 config.window_decorations = "RESIZE"
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
 
 config.leader = {
   key = "a",
